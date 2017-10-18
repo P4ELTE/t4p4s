fb958bb7001448a8731ee359a49f1e6c6522fed2
Kitlei Róbert <kitlei@elte.hu>
2017-10-18 02:21:17 +0200

    Improve compiler.py: indentation, desugaring in src/utils

--- src/compiler.py
+++ src/compiler.py
@@ -43,30 +43,58 @@ hlir = None
 hlir16 = None
 
 
-def translate_line_with_insert(file, line_idx, line):
+indentation_level = 0
+
+def translate_line_with_insert(file, line_idx, line, indent_str):
     """Gets a line that contains an insert
        and transforms it to a Python code section."""
     # since Python code is generated, indentation has to be respected
-    indentation = re.sub(r'^([ \t]*)#\[.*$', r'\1', line)
+    indentation = re.sub(r'^([ \t]*)#[\[\{\}].*$', r'\1', line)
 
-    # get the #[ part
-    content = re.sub(r'^[ \t]*#\[([ \t]*[^\n]*)[ \t]*', r'\1', line)
+    global indentation_level
+    pre_indentation_mod = ""
+    post_indentation_mod = ""
+    # #{ unindents starting this line
+    if '#}' in line:
+        if indentation_level == 0:
+            addError("Compiler", "Too much unindent in {}:{}".format(file, line_idx))
+        indentation_level -= 1
+        pre_indentation_mod = indentation + "file_indentation_level -= 1\n"
+
+    # #{ starts a new indentation level from the next line
+    if '#{' in line:
+        indentation_level += 1
+        post_indentation_mod = "\n" + indentation + "file_indentation_level += 1"
+
+    # get the #[ (or #{, or #}) part
+    content = re.sub(r'^[ \t]*#[\[\{\}]([ \t]*[^\n]*)[ \t]*', r'\1', line)
     # escape sequences like \n may appear in #[ parts
     content = re.sub(r'\\', r'\\\\', content)
     # quotes may appear in #[ parts
     content = re.sub(r'"', r'\"', content)
     # replace ${var} and ${call()} inserts
     content = re.sub(r'\${([ \t\f\v]*)([^}]+)([ \t\f\v]*)}', r'" + str(\2) + "', content)
+    # replace $var inserts
+    content = re.sub(r'\$([a-zA-Z0-9_]*)', r'" + str(\1) + "', content)
+    # trim the line
+    content = content.strip()
 
     # add a comment that shows where the line is generated at
     is_nonempty_line = bool(content.strip())
     if is_nonempty_line and line_idx is not None:
         if args['desugar_info'] == "comment":
-            content += " // sugar@%d" % (line_idx)
+            content += '" + sugar("{}", {}) + "'.format(os.path.basename(file), line_idx)
+            # content += " // {}@{}".format(os.path.basename(file), line_idx)
         if args['desugar_info'] == "pragma":
             content = '#line %d \\"%s\\"\\n%s' % (line_idx, "../../" + file, content)
 
-    return indentation + "generated_code += \"" + content + "\\n\""
+    return '{}{}generated_code += indent() + "{}"{}'.format(pre_indentation_mod, indentation, content, post_indentation_mod)
+
+
+def increase(idx):
+    if idx is None:
+        return None
+    return idx + 1
 
 
 def add_empty_lines(code_lines):
@@ -89,27 +117,112 @@ def add_empty_lines(code_lines):
         else:
             if not line.strip():
                 continue
-            new_lines.append((idx + 1, line))
+            new_lines.append((increase(idx), line))
             last_indent = len(line) - len(line.lstrip())
             already_added = False
 
     return new_lines
 
 
-def translate_file_contents(file, code):
-    """Returns the code transformed into runnable Python code.
-       Translated are #[generated_code and ${var} constructs."""
-    has_translatable_comment = re.compile(r'^[ \t]*#\[[ \t]*.*$')
-
+def add_gen_in_def(code_lines):
+    """If a function's name starts with 'gen_' in a generated file,
+    that function produces code.
+    This is a helper function that initialises and returns the appropriate variable.
+    Also, if "return" is encountered on a single line,
+    the requisite return value is inserted."""
     new_lines = []
+    is_inside_gen = False
+    for idx, line in code_lines:
+        if is_inside_gen:
+            if re.match(r'^[ \t]*return[ \t]*$', line):
+                line = re.sub(r'^([ \t]*)return[ \t]*$', r'\1return generated_code', line)
+
+            is_separator_line  = re.match(r'^#[ \t]*([^ \t])\1\1*', line)
+            is_method_line     = re.sub(r'[ \t]*#.*', '', line).strip() != "" and line.lstrip() == line
+            is_unindented_line = re.match(r'^[^ \t]', line)
+            if is_separator_line or is_method_line or is_unindented_line:
+                new_lines.append((None, '    return generated_code'))
+                new_lines.append((None, ''))
+                is_inside_gen = False
+
+        if line.startswith('def gen_'):
+            new_lines.append((idx, line))
+            new_lines.append((None, '    generated_code = ""'))
+            is_inside_gen = True
+            continue
+
+        new_lines.append((idx, line))
+
+    if is_inside_gen:
+        addError("Compiler", "generator method at the end of file {}".format(file))
+
+    return new_lines
+
+
+def translate_file_contents(file, code, indent_str="    "):
+    """Returns the code transformed into runnable Python code.
+       Translated are #[generated_code, #=generator_expression and ${var} constructs."""
+    has_translatable_comment = re.compile(r'^[ \t]*#[\[\{\}][ \t]*.*$')
+
+    global indentation_level
+    indentation_level = 0
+
+    new_lines = """
+# Autogenerated file (from {}), do not modify directly.
+
+global file_indentation_level
+file_indentation_level = 0
+
+# The last element is the innermost (current) style.
+file_sugar_style = ['line_comment']
+
+
+def indent():
+    global file_indentation_level
+    return '{}' * file_indentation_level
+
+
+class SugarStyle():
+    def __init__(self, sugar):
+        global file_sugar_style
+        file_sugar_style.append(sugar)
+
+    def __enter__(self):
+        global file_sugar_style
+        return file_sugar_style[-1]
+
+    def __exit__(self, type, value, traceback):
+        global file_sugar_style
+        file_sugar_style.pop()
+
+
+def sugar(file, line):
+    global file_sugar_style
+    if file_sugar_style[-1] == 'line_comment': return ' // ' + file + '@' + str(line) + '\\n'
+    if file_sugar_style[-1] == 'inline_comment': return '/* ' + file + '@' + str(line) + '*/'
+    return ''
+
+
+""".format(file, indent_str).splitlines()
+
     code_lines = enumerate(code.splitlines())
-    code_lines2 = add_empty_lines(code_lines)
-    for idx, code_line in code_lines2:
+    code_lines = add_gen_in_def(code_lines)
+    code_lines = add_empty_lines(code_lines)
+    for idx, code_line in code_lines:
         new_line = code_line
         if has_translatable_comment.match(code_line):
-            new_line = translate_line_with_insert(file, idx, code_line)
+            new_line = translate_line_with_insert(file, idx, code_line, indent_str)
+        elif re.match(r'^[ \t]*#= .*$', code_line):
+            new_line = re.sub(r'^([ \t]*)#=(.*)$', r'\1generated_code += str(\2)', code_line)
+
+            if args['desugar_info'] == "comment":
+                new_line += " # {}@{}".format(os.path.basename(file), idx)
 
         new_lines.append(new_line)
+
+    if indentation_level != 0:
+        addError("Compiler", "Non-zero indentation level ({}) at end of file: {}".format(indentation_level, file))
+
     return '\n'.join(new_lines)
 
 
@@ -149,6 +262,19 @@ def generate_code(file, genfile, localvars={}):
                 raise
 
         return re.sub(r'\n{3,}', '\n\n', localvars['generated_code'])
+
+
+def generate_desugared_src(hlir, hlir16):
+    """Some Python source files also use the sugared syntax.
+    The desugared files are generated here."""
+    import glob
+    for fromfile in glob.glob("src/utils/*.sugar.py"):
+        tofile = re.sub("[.]sugar[.]py$", ".py", fromfile)
+        with open(fromfile, "r") as orig_file:
+            code = orig_file.read()
+            code = translate_file_contents(fromfile, code)
+
+            write_file(tofile, code)
 
 
 def generate_desugared(hlir, hlir16, filename, file_with_path):
@@ -277,6 +403,7 @@ def main():
         if not file_with_path.endswith(".c.py") and not file_with_path.endswith(".h.py"):
             continue
 
+        generate_desugared_src(hlir, hlir16)
         generate_desugared(hlir, hlir16, filename, file_with_path)
 
     showErrors()
