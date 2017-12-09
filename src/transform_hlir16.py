9aaf61ae6afa00b6908985f0ac8b01282b574990
Unknown <ldani@elte.hu>
2017-12-09 23:31:26 +0100

    Annotations now passing extra parameters to extern function calls

--- src/transform_hlir16.py
+++ src/transform_hlir16.py
@@ -13,9 +13,23 @@
 # limitations under the License.
 #!/usr/bin/env python
 
+from hlir16.p4node import P4Node, deep_copy
+from hlir16.hlir16 import get_fresh_node_id
+
 def apply_annotations(postfix, extra_args, x):
     if (x.methodCall.method.node_type=="PathExpression") :
-	x.methodCall.method.ref.name += "_" + postfix
+        new_decl = deep_copy(x.methodCall.method.ref)
+        new_decl.name += "_" + postfix
+        extra_args_types = [ P4Node({'id' : get_fresh_node_id(),
+                                   'node_type' : 'Parameter',
+                                   'type' : arg.type,
+                                   'name' : postfix + "_extra_param",
+                                   'direction' : 'in',
+                                   'declid' : 0,         #TODO: is this important?
+                                   'annotations' : []
+                                   }) for arg in extra_args ]
+        new_decl.type.parameters.parameters.vec += extra_args_types
+        x.methodCall.method.ref = new_decl
         x.methodCall.method.path.name += "_" + postfix
         x.methodCall.arguments.vec += extra_args
     return x
@@ -24,8 +38,10 @@ def search_for_annotations(x):
     available_optimization_annotations = ['offload']
 
     if (x.node_type == "BlockStatement") :
-        name_list = [annot.name for annot in x.annotations.annotations.vec if annot.name in  available_optimization_annotations]
-        arg_list = [annot.expr for annot in x.annotations.annotations.vec if annot.name in  available_optimization_annotations]
+        name_list = [annot.name for annot in x.annotations.annotations.vec if annot.name in available_optimization_annotations]
+        arg_list = []
+        for annot in x.annotations.annotations.vec :
+            if annot.name in available_optimization_annotations : arg_list += annot.expr
         if ([] != name_list) :
 	    x.components = map(lambda x : apply_annotations('_'.join(name_list), arg_list, x), x.components)
     return x
