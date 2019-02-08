37f2eff0564d000e2a7f1c8357bb684d4ffe70bb
Kitlei Róbert <kitlei@elte.hu>
2019-02-08 16:24:48 +0100

    Add emit extern

---
+++ src/hardware_dep/dpdk/data_plane/dpdkx_emit.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2018 Eotvos Lorand University, Budapest, Hungary
+
+#include "dpdk_lib.h"
+
+void extern_packet_out_emit() {
+    debug("Emit extern...\n");
+}
