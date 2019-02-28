47dba9e5ed792e994d425263532ba095ae2684da
Kitlei Róbert <kitlei@elte.hu>
2019-02-28 08:29:10 +0100

    Remove debug printout

--- src/hardware_dep/dpdk/data_plane/dpdk_lib_init_tables.c
+++ src/hardware_dep/dpdk/data_plane/dpdk_lib_init_tables.c
@@ -49,7 +49,6 @@ void create_table_on_lcore(unsigned lcore_id)
     // TODO is it necessary to store the table in two places?
     for (int i = 0; i < NB_TABLES; i++) {
         struct lcore_conf* qconf = &lcore_conf[lcore_id];
-        debug("dbg tblprint %p %p\n", qconf->state.tables[i], state[socketid].tables[i][0]);
         qconf->state.tables[i] = state[socketid].tables[i][0];
     }
 }
