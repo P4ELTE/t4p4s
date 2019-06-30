f0d14475b7eadfbabe1ea8b0b23788a0bc4da898
Kitlei Róbert <kitlei@elte.hu>
2019-06-30 07:52:07 +0200

    Add common.h with metadata

---
+++ src/hardware_indep/common.h.py
@@ -0,0 +1,24 @@
+#[ // SPDX-License-Identifier: Apache-2.0
+#[ // Copyright 2019 Eotvos Lorand University, Budapest, Hungary
+
+from hlir16.utils_hlir16 import *
+from utils.codegen import format_type
+
+
+#[ #ifndef __COMMON_H__
+#[ #define __COMMON_H__
+
+for typedef in hlir16.objects['Type_Typedef']:
+    #[ typedef ${format_type(typedef.type)} ${typedef.name};
+
+for struct in hlir16.objects['Type_Struct']:
+    # TODO make condition less arbitrary
+    if not struct.name.endswith('_t'):
+        continue
+
+    #{ struct ${struct.name} {
+    for field in struct.fields:
+        #[ ${format_type(field.type)} ${field.name};
+    #} };
+
+#[ #endif // __COMMON_H__
