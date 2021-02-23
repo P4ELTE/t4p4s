8ddbd49290abfcef0c23d499ba0d669ccf743f17
Kitlei Róbert <kitlei@elte.hu>
2021-02-23 09:43:34 +0100

    Add command line options

--- src/hardware_dep/shared/util_debug.c
+++ src/hardware_dep/shared/util_debug.c
@@ -11,11 +11,16 @@
     pthread_mutex_t dbg_mutex;
 
     void dbg_fprint_bytes(FILE* out_file, void* bytes, int byte_count) {
-        int reasonable_upper_limit = 160;
+        #ifndef T4P4S_DEBUG_PKT_MAXBYTES
+            int reasonable_upper_limit = 80;
+        #else
+            int reasonable_upper_limit = T4P4S_DEBUG_PKT_MAXBYTES;
+        #endif
+
         if (byte_count <= 0)    return;
 
         if (byte_count > reasonable_upper_limit) {
-            fprintf(out_file, "(%d bytes, showing first %d) ", byte_count, reasonable_upper_limit);
+            fprintf(out_file, "(showing %dB) ", reasonable_upper_limit);
         }
 
         for (int i = 0; i < (byte_count <= reasonable_upper_limit ? byte_count : reasonable_upper_limit); ++i) {
