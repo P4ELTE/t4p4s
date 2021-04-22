db1fb0ac381cec76c5b65528fe35f214052787b3
Kitlei Róbert <kitlei@elte.hu>
2021-04-22 12:04:39 +0200

    Add example test files

---
+++ examples/test/test-switch-translated-to-p4-16.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Eotvos Lorand University, Budapest, Hungary
+
+#include "test.h"
+
+fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] = {
+    {
+        {FAKE_PKT, 0, 1, ETH(ETH01, ETH1A, "0000000000000000000000000000000000000000"), NO_CTL_REPLY, 11, ETH(ETH01, ETH1A, "0000000000000000000000000000000000000000")},
+        FEND,
+    },
+
+    {
+        FEND,
+    },
+};
+
+testcase_t t4p4s_test_suite[MAX_TESTCASES] = {
+    { "test",           &t4p4s_testcase_test },
+    TEST_SUITE_END,
+};
