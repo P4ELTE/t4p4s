76839e7ae22fd10b3b349120d81d99259245374f
Kitlei Róbert <kitlei@elte.hu>
2021-04-22 12:00:14 +0200

    Add externs: clone3, hash, random_fun

---
+++ src/hardware_dep/dpdk/data_plane/dpdkx_random_fun.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Eotvos Lorand University, Budapest, Hungary
+
+#include "dpdk_model_v1model.h"
+#include "util_packet.h"
+#include "util_debug.h"
+
+#include "dpdk_lib.h"
+#include "stateful_memory.h"
+
+#include <rte_ip.h>
+
+
+void random_fun(int* out, int min, int max, SHORT_STDPARAMS) {
+    // TODO implement
+    debug("    : Called random_fun");
+}
---
+++ src/hardware_dep/dpdk/includes/dpdkx_random_fun.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2021 Eotvos Lorand University, Budapest, Hungary
+
+#pragma once
+
+#include "dpdk_lib.h"
+
+void random_fun(int* out, int min, int max, SHORT_STDPARAMS);
