79bb5102c3ad6aa0a236cceeea68536404db22dc
Kitlei Róbert <kitlei@elte.hu>
2021-04-22 12:00:15 +0200

    Add support for header stacks

--- src/hardware_dep/shared/dataplane_hdr_fld_pkt.c
+++ src/hardware_dep/shared/dataplane_hdr_fld_pkt.c
@@ -12,3 +12,20 @@ void deactivate_hdr(header_instance_t hdr, packet_descriptor_t* pd) {
     pd->headers[hdr].pointer = 0;
     pd->is_emit_reordering = true;
 }
+
+void stk_next(header_stack_t stk, packet_descriptor_t* pd) {
+    ++pd->stacks[stk].current;
+}
+
+header_instance_t stk_at_idx(header_stack_t stk, int idx, packet_descriptor_t* pd) {
+    return stk_infos[stk].start_hdr + idx;
+}
+
+header_instance_t stk_current(header_stack_t stk, packet_descriptor_t* pd) {
+    return stk_at_idx(stk, pd->stacks[stk].current, pd);
+}
+
+field_instance_t stk_start_fld_idx(header_instance_t hdr) {
+	stk_info_t infos = stk_infos[hdr];
+    return infos.start_fld_idx + (hdr - infos.start_hdr) * infos.fld_count;
+}
