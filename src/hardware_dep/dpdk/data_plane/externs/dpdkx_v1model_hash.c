f688a39ea77709f26d51ce612f5d1ba4825d0515
Peter Hudoba <hudi1989@gmail.com>
2021-05-07 09:39:54 +0200

    remove old unused externs move to new place

--- src/hardware_dep/dpdk/data_plane/externs/dpdkx_v1model_hash.c
+++ src/hardware_dep/dpdk/data_plane/externs/dpdkx_v1model_hash.c
@@ -8,10 +8,25 @@
 #include "dpdk_lib.h"
 #include "stateful_memory.h"
 
+#include "rte_hash_crc.h"
+
 #include <rte_ip.h>
 
 void hash(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
     debug("    : Executing hash\n");
+}
+
+void hash__u32__u16__bufs__u32(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
+    dbg_bytes(data.buffer, data.buffer_size, "    : Executing hash on " T4LIT(%d) " bytes: ", data.buffer_size);
+    if (hash == enum_HashAlgorithm_crc32){
+        uint32_t crc32_result = rte_hash_crc(data.buffer, data.buffer_size, 0xffffffff);
+        memcpy(result, &crc32_result, 4);
+    } else if (hash == enum_HashAlgorithm_identity) {
+        memcpy(result, data.buffer, data.buffer_size > 8 ? 8 : data.buffer_size);
+    } else {
+        debug("    : Unknown hashing method!\n");
+    }
+    dbg_bytes(result, 4, "        Result:");
 }
 
 void hash__u16__u16__u16s__u32(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
