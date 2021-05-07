2c401307f9cc640d86345564a63eb6baaefa27b7
Peter Hudoba <hudi1989@gmail.com>
2021-05-07 11:38:40 +0200

    add hash algorithms identity, random and refactor

--- src/hardware_dep/dpdk/data_plane/externs/dpdkx_v1model_hash.c
+++ src/hardware_dep/dpdk/data_plane/externs/dpdkx_v1model_hash.c
@@ -8,7 +8,8 @@
 #include "dpdk_lib.h"
 #include "stateful_memory.h"
 
-#include "rte_hash_crc.h"
+#include <rte_hash_crc.h>
+#include <rte_compat.h>
 
 #include <rte_ip.h>
 
@@ -16,16 +17,32 @@ void hash(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buff
     debug("    : Executing hash\n");
 }
 
-void hash__u32__u16__bufs__u32(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
+void hash__u32__u16__bufs__u32(uint32_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
     dbg_bytes(data.buffer, data.buffer_size, "    : Executing hash on " T4LIT(%d) " bytes: ", data.buffer_size);
-    if (hash == enum_HashAlgorithm_crc32){
-        uint32_t crc32_result = rte_hash_crc(data.buffer, data.buffer_size, 0xffffffff);
-        memcpy(result, &crc32_result, 4);
-    } else if (hash == enum_HashAlgorithm_identity) {
-        memcpy(result, data.buffer, data.buffer_size > 8 ? 8 : data.buffer_size);
-    } else {
-        debug("    : Unknown hashing method!\n");
+
+    switch(hash){
+        case enum_HashAlgorithm_crc32:
+            {
+                *result = rte_hash_crc(data.buffer, data.buffer_size, 0xffffffff);
+            };
+            break;
+        case enum_HashAlgorithm_identity:
+            {
+                memcpy(result, data.buffer, data.buffer_size > 4 ? 4 : data.buffer_size);
+            }
+            break;
+        case enum_HashAlgorithm_random:
+            {
+                *result = rte_rand();
+            }
+            break;
+        default:
+            {
+                debug("    " T4LIT(Not implemented hash algorithm!,error) " fallback to identity algorithm" "\n");
+                memcpy(result, data.buffer, data.buffer_size > 4 ? 4 : data.buffer_size);
+            }
     }
+
     dbg_bytes(result, 4, "        Result:");
 }
 
@@ -38,5 +55,28 @@ void hash__u16__u16__u32s__u32(uint16_t* result, enum_HashAlgorithm_t hash, uint
 }
 
 void hash__u16__u16__bufs__u32(uint16_t* result, enum_HashAlgorithm_t hash, uint16_t base, uint8_buffer_t data, uint32_t max, SHORT_STDPARAMS) {
-    debug("    : Executing hash__u16__u16__ubufs__u32\n");
+    dbg_bytes(data.buffer, data.buffer_size, "    : Executing hash on " T4LIT(%d) " bytes: ", data.buffer_size);
+    switch(hash) {
+        case enum_HashAlgorithm_csum16:
+            {
+                *result = rte_raw_cksum(data.buffer, data.buffer_size);
+            }
+            break;
+        case enum_HashAlgorithm_identity:
+            {
+                memcpy(result, data.buffer, data.buffer_size > 2 ? 2 : data.buffer_size);
+            }
+            break;
+        case enum_HashAlgorithm_random:
+            {
+                *result = rte_rand();
+            }
+            break;
+        default:
+            {
+                debug("    " T4LIT(Not implemented hash algorithm!,error) " fallback to identity algorithm" "\n");
+                memcpy(result, data.buffer, data.buffer_size > 2 ? 2 : data.buffer_size);
+            }
+    }
+    dbg_bytes(result, 2, "        Result:");
 }
