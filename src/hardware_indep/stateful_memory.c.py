4fbaf9f159b3f3c710d475afbc8a7a0b14fd73be
Kitlei Róbert <kitlei@elte.hu>
2021-09-03 07:01:08 +0200

    Improve software memory handling

--- src/hardware_indep/stateful_memory.c.py
+++ src/hardware_indep/stateful_memory.c.py
@@ -2,10 +2,21 @@
 # Copyright 2018 Eotvos Lorand University, Budapest, Hungary
 
 from utils.codegen import format_expr, format_statement, format_declaration
+from compiler_common import unique_everseen
 
 #[ #include "stateful_memory.h"
 
 #[ global_state_t global_smem;
+
+for stype in unique_everseen(smem.smem_type for table, smem in hlir.all_meters + hlir.all_counters):
+    #{ void init_${stype}_smem(${stype}_t smem[], int* global_smem_amount_ref, int amount, const char*const name, const char*const purpose) {
+    #{     for (int idx = 0; idx < amount; ++idx) {
+    #[         sprintf(smem[idx].name, "%s/%s", name, purpose);
+    #}     }
+
+    #[     *global_smem_amount_ref = amount;
+    #} }
+    #[
 
 #{ void gen_init_smems() {
 #[ #ifdef T4P4S_DEBUG
@@ -18,24 +29,20 @@ for table, smem in hlir.all_meters + hlir.all_counters:
         for c in smem.components:
             cname = c['name']
             if not smem.is_direct:
-                #{ for (int idx = 0; idx < ${smem.amount}; ++idx) {
-                #[     strcpy(global_smem.${cname}[idx].name, "${smem.name}/${c['for']}");
-                #} }
-
-                #[ global_smem.${cname}_amount = ${smem.amount};
+                #[     init_${smem.smem_type}_smem(global_smem.${cname}, &global_smem.${cname}_amount, ${smem.amount}, "${smem.name}", "${c['for']}");
             else:
-                #[ strcpy(global_smem.${cname}_${table.name}.name, "${smem.name}/${c['for']}");
+                #[     strcpy(global_smem.${cname}_${table.name}.name, "${smem.name}/${c['for']}");
 
     #[
 
 for smem in hlir.registers:
     for c in smem.components:
         cname = c['name']
-        #{ for (int idx = 0; idx < ${smem.amount}; ++idx) {
-        #[     strcpy(global_smem.${smem.smem_type}_${cname}[idx].name, "${smem.name}");
-        #} }
+        #{     for (int idx = 0; idx < ${smem.amount}; ++idx) {
+        #[         strcpy(global_smem.${smem.smem_type}_${cname}[idx].name, "${smem.name}");
+        #}     }
 
-        #[ global_smem.${smem.smem_type}_${smem.name}_amount = ${smem.amount};
+        #[     global_smem.${smem.smem_type}_${smem.name}_amount = ${smem.amount};
 
     #[
 
