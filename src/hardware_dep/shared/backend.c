77fc9c118b75509eee41f685c194fc5708c2c975
Kitlei Róbert <kitlei@elte.hu>
2021-09-03 07:01:08 +0200

    Minor changes

--- src/hardware_dep/shared/backend.c
+++ src/hardware_dep/shared/backend.c
@@ -3,6 +3,6 @@
 
 #include "backend.h"
 
-bool is_header_valid(header_instance_t hdr, packet_descriptor_t* pd) {
+bool is_header_valid(header_instance_e hdr, packet_descriptor_t* pd) {
     return pd->headers[hdr].pointer != NULL;
 }
