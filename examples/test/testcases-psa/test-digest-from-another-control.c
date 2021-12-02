e19ac5de40f4845eb2a80793f0bd741582c169dc
Kitlei Róbert <kitlei@elte.hu>
2021-12-02 12:40:13 +0100

    Add test cases

---
+++ examples/test/testcases-psa/test-digest-from-another-control.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2018 Eotvos Lorand University, Budapest, Hungary
+
+#include "test.h"
+
+fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] = SINGLE_LCORE(
+    FAST(0, 0, "01"),
+    FAST(0, 0, "02")
+    );
+
+testcase_t t4p4s_test_suite[MAX_TESTCASES] = {
+    { "test",           &t4p4s_testcase_test },
+    TEST_SUITE_END,
+};
---
+++ examples/test/testcases-psa/test-smem-psa.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2018 Eotvos Lorand University, Budapest, Hungary
+
+#include "test.h"
+
+fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] =
+    SINGLE_LCORE(
+        FAST(0, 12345, hETH4(ETH1A, ETH01))
+    );
+
+
+testcase_t t4p4s_test_suite[MAX_TESTCASES] = {
+    { "test",           &t4p4s_testcase_test, "psa"  },
+    TEST_SUITE_END,
+};
