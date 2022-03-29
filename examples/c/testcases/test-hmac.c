6f7c3a87daff35d35dda61ffeaa85c70bc609a81
Peter Hudoba <hudi1989@gmail.com>
2022-03-29 20:36:48 +0200

    feat (crypto ipsec): finish crypto and hmac calulations

--- examples/c/testcases/test-hmac.c
+++ examples/c/testcases/test-hmac.c
@@ -6,17 +6,20 @@
 fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] =
     SINGLE_LCORE(
         FAST(0, 0, "00", "68656c6c6f776f726c64617364313233", OUT("3d770bc1f907b2d3315edf7129b2245b")),
-        FAST(0, 0, "00", "68656c6c6f776f726c64617364", OUT(c0x3B), OUT("38886a7333ce4e56aee3731e115b7343")),
-        FAST(0, 0, "00", "68656c6c6f776f726c64", OUT(c0x6B), OUT("a3a7d3996f5c23ce404c3588a182314f")),
-        FAST(0, 0, "00", "68656c6c6f", OUT(c0x11B), OUT("fac7c5ceca254536550cdd5271448a49")),
-        FAST(0, 0, "01", "68656c6c6f776f726c64617364313233", OUT("00"), OUT("4181d3eb5ec1b6e2873c29d5eeeccbfa")),
-        FAST(0, 0, "01", "68656c6c6f776f726c64617364", OUT(c0x4B), OUT("67c11c0e82197607ba6a5a6e92a11a75")),
-        FAST(0, 0, "01", "68656c6c6f776f726c64", OUT(c0x7B), OUT("14349a8d4a8d27f374c3a5b5f38f3b95")),
-        FAST(0, 0, "01", "68656c6c6f", OUT(c0x12B), OUT("a0c850de60ff6b5e4e27a41c7afd9c63")),
-        FAST(0, 0, "03", "aaaaaa68656c6c6f", OUT(c0x11B), OUT("fac7c5ceca254536550cdd5271448a49")),
-        FAST(0, 0, "05", "bbbbaaaaaa68656c6c6f", OUT(c0x11B), OUT("fac7c5ceca254536550cdd5271448a49")),
-        FAST(0, 0, "06", "ccbbbbaaaaaa68656c6c6f", OUT(c0x11B), OUT("fac7c5ceca254536550cdd5271448a49")),
-        FAST(0, 0, "0a", "ddddddddccbbbbaaaaaa68656c6c6f", OUT(c0x11B), OUT("fac7c5ceca254536550cdd5271448a49"))
+        FAST(0, 0, "00", "68656c6c6f776f726c64617364", OUT("38886a7333ce4e56aee3731e115b7343")),
+        FAST(0, 0, "00", "68656c6c6f776f726c64", OUT("a3a7d3996f5c23ce404c3588a182314f")),
+        FAST(0, 0, "00", "68656c6c6f", OUT("fac7c5ceca254536550cdd5271448a49")),
+        FAST(0, 0, "01", "68656c6c6f776f726c64617364313233", OUT("4181d3eb5ec1b6e2873c29d5eeeccbfa")),
+        FAST(0, 0, "01", "68656c6c6f776f726c64617364",  OUT("67c11c0e82197607ba6a5a6e92a11a75")),
+        FAST(0, 0, "01", "68656c6c6f776f726c64",  OUT("14349a8d4a8d27f374c3a5b5f38f3b95")),
+        FAST(0, 0, "01", "68656c6c6f", OUT("a0c850de60ff6b5e4e27a41c7afd9c63")),
+        FAST(0, 0, "03", "aaaaaa68656c6c6f",  OUT("fac7c5ceca254536550cdd5271448a49")),
+        FAST(0, 0, "05", "bbbbaaaaaa68656c6c6f", OUT("fac7c5ceca254536550cdd5271448a49")),
+        FAST(0, 0, "06", "ccbbbbaaaaaa68656c6c6f", OUT("fac7c5ceca254536550cdd5271448a49")),
+        FAST(0, 0, "0a", "ddddddddccbbbbaaaaaa68656c6c6f", OUT("fac7c5ceca254536550cdd5271448a49")),
+
+        FAST(0,0, "2100","ddddddddddddeeeeeeeeeeee08004500001600010000400074e2010101010202020200000222000000010123456701234567012345670123456795ad5d83be4a49a5bf9e27bf63533ca7b1a59daaa8b167f3f2fcb7473d74ee50",
+              OUT("947aa87dcd07127351adac3d8f274149"))
     );
 
 testcase_t t4p4s_test_suite[MAX_TESTCASES] = {
--- src/hardware_dep/dpdk/data_plane/externs/dpdkx_crypto_ipsec.c
+++ src/hardware_dep/dpdk/data_plane/externs/dpdkx_crypto_ipsec.c
@@ -21,73 +21,93 @@ extern void do_crypto_operation(crypto_task_type_e task_type, int offset, SHORT_
 // -----------------------------------------------------------------------------
 // Implementation of P4 architecture externs
 
-void ipsec_prepare_encrypt_msg(int headers_size, int pad_length_size, int next_header_size, int esp_size, int iv_size, int wrapper_size, int original_payload_size, int padding_size, int to_encrypt_size, SHORT_STDPARAMS) {
-    rte_pktmbuf_append(pd->wrapper,esp_size + iv_size + to_encrypt_size - original_payload_size);
-    uint8_t* data = rte_pktmbuf_mtod(pd->wrapper, uint8_t*);
+void EXTERNIMPL0(ipsec_encapsulate)(SHORT_STDPARAMS) {
+    int wrapper_size = rte_pktmbuf_pkt_len(pd->wrapper);
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), wrapper_size, "Starting IPsec (" T4LIT(%dB) "): ", wrapper_size);
 
-    uint8_t* data_pointer = data;
-    // Pass the existing header and leave space for the ESP and IV in payload (we forget about the original payload)
-    data_pointer += headers_size + esp_size + iv_size;
+    const uint32_t eth_length = 14;
+    const uint32_t ip_length = 20;
+
+    // Tunnel mode: we encrypt everything after eth header
+    const uint32_t length_to_encrypt_from_original = wrapper_size - eth_length;
+    const uint32_t payload_length =  wrapper_size - eth_length - ip_length;
+    const uint32_t esp_trail_length = 2;
+    const uint32_t padding_size = (16 - (length_to_encrypt_from_original + esp_trail_length) % 16) % 16;
+    const uint32_t to_encrypt_size = length_to_encrypt_from_original + padding_size + esp_trail_length;
+
+    const uint32_t esp_head_size = 8;
+    const uint32_t iv_size = 16;
+
+    const uint32_t total_HMAC_length = 16;
+    const uint32_t kept_HMAC_length = 12;
+
+    fprintf(stderr,"_------>wrapper_size: %d\n",wrapper_size);
+    fprintf(stderr,"_------>esp_head_size: %d\n",esp_head_size);
+    fprintf(stderr,"_------>iv_size: %d\n",iv_size);
+    fprintf(stderr,"_------>to_encrypt_size: %d\n",to_encrypt_size);
+    fprintf(stderr,"_------>payload_length: %d\n",payload_length);
+    fprintf(stderr,"_------>esp_head_size + iv_size + to_encrypt_size - payload_length: %d\n",esp_head_size + iv_size + to_encrypt_size - payload_length);
+
+    rte_pktmbuf_append(pd->wrapper, esp_head_size + iv_size + to_encrypt_size - payload_length);
+    debug("   :: Added " T4LIT(%dB) " of " T4LIT(padding,field) ", " T4LIT(1B) " of " T4LIT(pad length size,field) ", and " T4LIT(1B) " of " T4LIT(next header size,field) " for a total of " T4LIT(%dB) "\n", padding_size, to_encrypt_size);
+
+    uint8_t* wrapper_pointer = rte_pktmbuf_mtod(pd->wrapper, uint8_t*);
+
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "Actual wrapper (" T4LIT(%dB) "): ", rte_pktmbuf_pkt_len(pd->wrapper));
+    debug("   :: Copy " T4LIT(%dB) " data from wrapper \n", to_encrypt_size);
     // Important to use memmove, because the source and destination can overlap
-    memmove(data_pointer, data, wrapper_size);
+    memmove( wrapper_pointer + eth_length + ip_length + esp_head_size + iv_size, wrapper_pointer + eth_length, to_encrypt_size);
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "Actual wrapper (" T4LIT(%dB) "): ", rte_pktmbuf_pkt_len(pd->wrapper));
 
-    // Run to the end of the copied header+payload
-    data_pointer += wrapper_size;
+    uint8_t* data_pointer = wrapper_pointer + eth_length + ip_length;
+    // ESP (SPI + Sequence number)
+    *data_pointer++ = 0;
+    *data_pointer++ = 0;
+    *data_pointer++ = 0x02;
+    *data_pointer++ = 0x22;
+
+    // Sequence number
+    *data_pointer++ = 0;
+    *data_pointer++ = 0;
+    *data_pointer++ = 0;
+    *data_pointer++ = 1;
+
+    //IV
+    for(int a=0;a<16;a++){
+        *data_pointer++ = 0x00;
+    }
+    // Jump over the payload
+    data_pointer += length_to_encrypt_from_original;
     // And set the padding bytes
-    memset(data_pointer, 0xef, padding_size);
+    uint8_t padding_value = 1;
+    for(int padding_iterator = 0; padding_iterator < padding_size; padding_iterator++){
+        *data_pointer = padding_value;
+        data_pointer++;
+        padding_value++;
+    }
+    //ESP TRAIL
+    *data_pointer++ = (uint8_t)padding_size; // Pad length
+    *data_pointer++ = 4; // Set next header to ipv4
 
-    // Save the size of padding
-    data_pointer += padding_size;
-    memset(data_pointer, (uint8_t)padding_size, 1);
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "Actual wrapper (" T4LIT(%dB) "): ", rte_pktmbuf_pkt_len(pd->wrapper));
 
-    // Set next header to ipv4
-    data_pointer += 1;
-    memset(data_pointer, 4, 1);
+    do_crypto_operation(CRYPTO_TASK_ENCRYPT, eth_length + ip_length + esp_head_size + iv_size, SHORT_STDPARAMS_IN);
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "Actual wrapper (" T4LIT(%dB) "): ", rte_pktmbuf_pkt_len(pd->wrapper));
+    debug("offset for hmac: %d\n",eth_length + ip_length)
+    do_crypto_operation(CRYPTO_TASK_MD5_HMAC, eth_length + ip_length, SHORT_STDPARAMS_IN);
 
-    // TODO: add ESP
-    // ESP
-    memset(data + headers_size, 0x1a, 8);
-    // TODO: use random IV
-    // IV
-    memset(data + headers_size + esp_size, 0x1b, 8);
-}
-
-void ipsec_adjust_result(int total_HMAC_length, int kept_HMAC_length, SHORT_STDPARAMS) {
     // We keep only 12 bytes from 16 byte HMAC
     rte_pktmbuf_trim(pd->wrapper, total_HMAC_length - kept_HMAC_length);
 
     pd->payload_size = rte_pktmbuf_pkt_len(pd->wrapper);
-    // TODO properly update pd so that it uses the new packet
-}
-
-void EXTERNIMPL0(ipsec_encapsulate)(SHORT_STDPARAMS) {
-    int headers_size = 34;
-    int pad_length_size = 1;
-    int next_header_size = 1;
-    int esp_size = 8;
-    int iv_size = 8;
-
-    int total_HMAC_length = 16;
-    int kept_HMAC_length = 12;
-
-    int wrapper_size = rte_pktmbuf_pkt_len(pd->wrapper);
-    int original_payload_size = wrapper_size - headers_size;
-    int padding_size = (16 - (wrapper_size + pad_length_size + next_header_size)% 16) % 16;
-    int to_encrypt_size = padding_size + wrapper_size + pad_length_size + next_header_size;
-
-    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), wrapper_size, "Starting IPsec (" T4LIT(%dB) "): ", wrapper_size);
-    debug("   :: Added " T4LIT(%dB) " of " T4LIT(padding,field) ", " T4LIT(1B) " of " T4LIT(pad length size,field) ", and " T4LIT(1B) " of " T4LIT(next header size,field) " for a total of " T4LIT(%dB) "\n", padding_size, to_encrypt_size);
-
-    ipsec_prepare_encrypt_msg(headers_size, pad_length_size, next_header_size, esp_size, iv_size, wrapper_size, original_payload_size, padding_size, to_encrypt_size, SHORT_STDPARAMS_IN);
-
-    do_crypto_operation(CRYPTO_TASK_ENCRYPT, headers_size + esp_size + iv_size, SHORT_STDPARAMS_IN);
-    do_crypto_operation(CRYPTO_TASK_MD5_HMAC, headers_size, SHORT_STDPARAMS_IN);
-
-    ipsec_adjust_result(total_HMAC_length, kept_HMAC_length, SHORT_STDPARAMS_IN);
+    //*((uint16_t*)(wrapper_pointer + eth_length + 3)) = (uint16_t )pd->payload_size - eth_length;
+    // TODO: use proper endian
+    *(wrapper_pointer + eth_length + 3) = (uint8_t )pd->payload_size - eth_length;
 
     int HMAC_offset = rte_pktmbuf_pkt_len(pd->wrapper) - kept_HMAC_length;
     dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "   :: IPsec done, keeping " T4LIT(%dB) " of " T4LIT(%dB) " " T4LIT(HMAC,field) " at offset " T4LIT(%d) ": ", kept_HMAC_length, total_HMAC_length, HMAC_offset);
     dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*) + HMAC_offset, kept_HMAC_length, "   :: IPsec done, keeping " T4LIT(%dB) " of " T4LIT(%dB) " " T4LIT(HMAC,field) " at offset " T4LIT(%d) ": ", kept_HMAC_length, total_HMAC_length, HMAC_offset);
+    dbg_bytes(rte_pktmbuf_mtod(pd->wrapper, uint8_t*), rte_pktmbuf_pkt_len(pd->wrapper), "   :: IPsec done, final result: ");
 }
 
 #endif
