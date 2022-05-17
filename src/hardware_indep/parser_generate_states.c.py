b45e5db2413f699afe3801bba67a4cfa010309ec
Kitlei Róbert <kitlei@elte.hu>
2022-05-17 12:20:27 +0200

    Transitions generator: add 4 stages

--- src/hardware_indep/parser_generate_states.c.py
+++ src/hardware_indep/parser_generate_states.c.py
@@ -6,6 +6,8 @@ import re
 
 from itertools import takewhile, dropwhile
 
+NOT_separator = '▶'
+
 for parser in hlir.parsers:
     def fmt_key(key):
         if key.node_type == 'DefaultExpression':
@@ -66,10 +68,10 @@ for parser in hlir.parsers:
                 elif len(infos) > 1:
                     keys = set(re.sub(r"[.].*", "", info) for info in infos)
                     if len(keys) == 1:
-                        joined_infos = ' '.join(re.sub(r"[^.]*[.]", ".", info) for info in infos)
+                        joined_infos = NOT_separator.join(re.sub(r"[^.]*[.]", ".", info) for info in infos)
                         prev_conds.append(f'{list(keys)[0]}.NOT({joined_infos})')
                     else:
-                        joined_infos = ' '.join(infos)
+                        joined_infos = NOT_separator.join(infos)
                         prev_conds.append(f'NOT({joined_infos})')
         else:
             next_state_name = state.selectExpression.path.name
@@ -93,20 +95,136 @@ for parser in hlir.parsers:
             key, *rest = txt.split("≠")
             return rest == [] or key not in keys
 
-        txts1 = list(takewhile(lambda txt: 'NOT' not in txt, txts))
-        txts2 = list(dropwhile(lambda txt: 'NOT' not in txt, txts))
-
-        keys = set(txt.split("=")[0] for txt in txts1)
-        return [txt for txt in txts1 if is_ok(txt, keys)] + txts2
+        keys = set(txt.split("=")[0] for txt in txts if 'NOT' not in txt)
+        return [txt for txt in txts if 'NOT' in txts or is_ok(txt, keys)]
 
     short_infos = [simplify_infos(info) for info in infos if info != []]
 
     if short_infos != []:
-        with open(os.path.join(targetdir, '..', f'parser_state_transitions_{parser.name}.txt'), 'w', encoding='utf8') as file:
+        with open(os.path.join(targetdir, '..', f'parser_state_transitions_{parser.name}_v1.txt'), 'w', encoding='utf8') as file:
             file.write('accepted: \n')
             for info in (info for info in short_infos if 'rejected' not in info):
                 file.write('    ' + ' '.join(info) + '\n')
             file.write('\n')
-            file.write('rejected: \n')
-            for info in (info for info in short_infos if 'rejected' in info):
-                file.write('    ' + ' '.join(info) + '\n')
+
+            if len(list((info for info in short_infos if 'rejected' in info))) != 0:
+                file.write('rejected: \n')
+                for info in (info for info in short_infos if 'rejected' in info):
+                    file.write('    ' + ' '.join(info) + '\n')
+
+    # v2
+    if short_infos != []:
+        with open(os.path.join(targetdir, '..', f'parser_state_transitions_{parser.name}_v2.txt'), 'w', encoding='utf8') as file:
+            file.write('accepted: \n')
+            collected = []
+            for info in (info for info in short_infos if 'rejected' not in info):
+                out_info = info
+                for idx in range(len(info), 1, -1):
+                    pre = info[:idx]
+                    if pre in collected:
+                        collected_idx = collected.index(pre)
+                        out_info = [f'@{collected_idx+1:04}'] + simplify_infos(info[idx:])
+                        break
+
+                this_except_last = info[:-1]
+                collected.append(this_except_last)
+
+                file.write(f'    {len(collected):04} ' + ' '.join(out_info) + '\n')
+            file.write('\n')
+
+            if len(list((info for info in short_infos if 'rejected' in info))) != 0:
+                file.write('rejected: \n')
+                for info in (info for info in short_infos if 'rejected' in info):
+                    file.write('    ' + ' '.join(info) + '\n')
+
+    # v3
+    if short_infos != []:
+        with open(os.path.join(targetdir, '..', f'parser_state_transitions_{parser.name}_v3.txt'), 'w', encoding='utf8') as file:
+            file.write('accepted: \n')
+            collected = {}
+            curr_idx = 1
+            for info in (info for info in short_infos if 'rejected' not in info):
+                out_info = info
+                for idx in range(len(info), 1, -1):
+                    pre = tuple(info[:idx])
+                    if pre in collected:
+                        collected_idx = collected[pre]
+                        out_info = [f'@{collected_idx}'] + simplify_infos(info[idx:])
+                        break
+                    else:
+                        collected[tuple(pre)] = f'{curr_idx:04}-{len(info)+1-idx}'
+
+                file.write(f'    {curr_idx:04} ' + ' '.join(out_info) + '\n')
+                curr_idx = curr_idx + 1
+            file.write('\n')
+
+            if len(list((info for info in short_infos if 'rejected' in info))) != 0:
+                file.write('rejected: \n')
+                for info in (info for info in short_infos if 'rejected' in info):
+                    file.write('    ' + ' '.join(info) + '\n')
+
+
+    def final_simplify_infos(in_infos, is_reject, postfixes):
+        prefixes = {}
+        curr_idx = 1
+        infos2 = []
+        for info in (info for info in in_infos if ('rejected' in info) == is_reject):
+            if tuple(info) in prefixes:
+                continue
+
+            out_info = info
+            for idx in range(len(info), 1, -1):
+                pre = tuple(info[:idx])
+                if pre in prefixes:
+                    prefixes_idx = prefixes[pre]
+                    out_info = [f'@{prefixes_idx}'] + simplify_infos(info[idx:])
+                    break
+                else:
+                    prefixes[tuple(pre)] = f'{curr_idx:04}+{idx}-{len(info)+1-idx}'
+
+            infos2.append(out_info)
+            curr_idx = curr_idx + 1
+
+        infos3 = []
+        postfix_idx = len(postfixes) + 1
+        for info in infos2:
+            if '@' not in info[0] or len(info) <= 2:
+                infos3.append(info)
+                continue
+
+            postfix = tuple(info[1:])
+            if postfix not in postfixes:
+                postfixes[postfix] = postfix_idx
+                postfix_idx = postfix_idx + 1
+
+            infos3.append([info[0], f'post@{postfixes[postfix]:04}'])
+
+        return infos3
+
+    def not_prefix_len(txts):
+        return -len(list(takewhile(lambda txt: all(not txt.startswith(pre) for pre in ('@', 'post@')), txts)))
+
+    def info_sorter(infos):
+        return sorted(enumerate(infos), key=lambda keyval: (not_prefix_len(keyval[1]), len(''.join(keyval[1])), keyval[0]))
+
+    # v4
+    if short_infos != []:
+        postfixes = {}
+        accepts = final_simplify_infos(short_infos, False, postfixes)
+        rejects = final_simplify_infos(short_infos, True, postfixes)
+
+        with open(os.path.join(targetdir, '..', f'parser_state_transitions_{parser.name}_v4.txt'), 'w', encoding='utf8') as file:
+            file.write('postfixes: \n')
+            for postfix, idx in sorted(postfixes.items(), key=lambda keyval: (-len(keyval[0]), -len(''.join(keyval[0])), keyval[1])):
+                file.write(f'    post@{idx:04} {" ".join(postfix)}\n')
+            file.write('\n')
+
+            file.write('accepted: \n')
+            for idx, info in info_sorter(accepts):
+                file.write(f'    {idx+1:04} {" ".join(info)}\n')
+            file.write('\n')
+
+            if len(list((info for info in short_infos if 'rejected' in info))) != 0:
+                file.write('rejected: \n')
+                for idx, info in info_sorter(rejects):
+                    file.write(f'    {idx+1:04} {" ".join(info)}\n')
