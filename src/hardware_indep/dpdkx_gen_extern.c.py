bbbdc7d877012f9389239a4de4498490166f2573
Sandor Laki <lakis@elte.hu>
2022-08-19 08:42:49 +0000

    Bugfix for checksum generation

--- src/hardware_indep/dpdkx_gen_extern.c.py
+++ src/hardware_indep/dpdkx_gen_extern.c.py
@@ -17,10 +17,15 @@ detailed_callinfos = get_detailed_extern_callinfos(hlir)
 def get_externtype(part, partypeinfolen, partype_suffix, varname):
     return f'EXTERNTYPE{partypeinfolen}({part}{partype_suffix})* {varname}'
 
-
 for partypeinfolen, mname_parts, partype_suffix, params, params_as_buf, ret, mname_postfix, mname_postfix_as_buf, args, args_as_buf, refvars, arginfos, parinfos in sorted(unique_everseen(detailed_callinfos, key=lambda c: c[0:3])):
     if len(mname_parts) == 1:
         call = f'SHORT_EXTERNCALL{partypeinfolen + len(mname_parts)-1}'
+        if 'GET32(src_pkt(pd)' in args_as_buf and mname_parts[0]=='update_checksum':
+            # TODO: fix this hack
+            atmp = args_as_buf.replace("GET32(src_pkt(pd),", "").split(',')
+            atmp[-4] = "checksum /*"
+            atmp[-3] = "*/"
+            args_as_buf = ','.join(atmp)
     else:
         call = f'EXTERNCALL{partypeinfolen + len(mname_parts)-2}'
         extern_type_name = f''
@@ -31,7 +36,6 @@ for partypeinfolen, mname_parts, partype_suffix, params, params_as_buf, ret, mna
 
     return_stmt = 'return ' if not ret.startswith('void') else ''
     #{ $ret $call(${",".join(mname_parts)}${partype_suffix})($params) {
-
     #[     ${return_stmt}EXTERNIMPL${partypeinfolen + len(mname_parts)-1}(${",".join(mname_parts)}${mname_postfix_as_buf})(${args_as_buf});
     #} }
     #[
